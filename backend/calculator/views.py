f46856f9d86636c36507259ada051e3a25e724cd
chelsieng <chelsiengmk@gmail.com>
2021-06-04 16:15:19 -0400

    basic calculations done

--- backend/calculator/views.py
+++ backend/calculator/views.py
@@ -1,9 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import simpleeval as se
+import re
 
 
-def function(request):
-    msg = """{
-            "value": "Hello World from Django!"
-        }"""
-    return HttpResponse(msg)
+def arccos(x):
+    return HttpResponse(x)
+
+
+def abPower(x):
+    return HttpResponse(x)
+
+
+def log(b, x):
+    return HttpResponse(b)
+
+
+def MAD(x):
+    return HttpResponse(x)
+
+
+def standardDeviation(x):
+    return HttpResponse(x)
+
+
+def sinh(x):
+    return HttpResponse(x)
+
+
+def xPower(request, x, y):
+    x ** y
+    return HttpResponse(x)
+
+
+def evaluate(request, expression):
+    # replace divide with /
+    expression = re.sub(r'divide', '/', expression)
+    ans = se.simple_eval(expression)
+    return HttpResponse(ans)
