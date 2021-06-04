9bd7a140f28c4e15956729a182569c1887ff0217
chelsieng <chelsiengmk@gmail.com>
2021-06-04 01:05:04 -0400

    UI prototype with UIKit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <h1>Eternity Calculator</h1>
   <Calculator/>
 </template>
 
--- frontend/src/components/Calculator.vue
+++ frontend/src/components/Calculator.vue
@@ -1,6 +1,103 @@
 <template>
-  <p>Calculator works!</p>
-  <p>{{ test }}</p>
+  <div class="uk-container-xsmall uk-align-center">
+    <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
+      <div>
+        <div class="uk-card uk-card-small uk-card-default uk-card-body uk-card-hover uk-text-right uk-text-lead">
+          {{ this.test }}
+        </div>
+      </div>
+    </div>
+    <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
+      <div>
+        <div class="uk-card uk-card-small uk-card-default uk-text-success uk-card-body uk-card-hover">AC</div>
+      </div>
+      <div>
+        <div class="uk-card uk-card-small uk-card-default uk-text-success uk-card-body uk-card-hover">arcos(x)</div>
+      </div>
+      <div>
+        <div class="uk-card uk-card-small uk-card-default uk-text-success uk-card-body uk-card-hover">sinh(x)</div>
+      </div>
+      <div>
+        <div class="uk-card uk-card-small uk-card-default uk-text-success uk-card-body uk-card-hover">log<sub>b</sub>(x)
+        </div>
+      </div>
+    </div>
+    <div class="uk-grid-small uk-child-width-expand@s uk-text-success uk-text-center" uk-grid>
+      <div>
+        <div class="uk-card uk-card-small uk-card-default uk-text-success uk-card-body uk-card-hover">MAD</div>
+      </div>
+      <div>
+        <div class="uk-card uk-card-small uk-card-default uk-text-success uk-card-body uk-card-hover">σ(x)</div>
+      </div>
+      <div>
+        <div class="uk-card uk-card-small uk-card-default uk-text-success uk-card-body uk-card-hover">ab<sup>x</sup>
+        </div>
+      </div>
+      <div>
+        <div class="uk-card uk-card-small uk-card-default uk-text-success uk-card-body uk-card-hover">x<sup>y</sup>
+        </div>
+      </div>
+    </div>
+    <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
+      <div>
+        <div class="uk-card uk-card-small uk-card-default uk-card-body uk-card-hover">7</div>
+      </div>
+      <div>
+        <div class="uk-card uk-card-small uk-card-default uk-card-body uk-card-hover">8</div>
+      </div>
+      <div>
+        <div class="uk-card uk-card-small uk-card-default uk-card-body uk-card-hover">9</div>
+      </div>
+      <div>
+        <div class="uk-card uk-card-small uk-card-default uk-text-warning uk-card-body uk-card-hover">&divide;</div>
+      </div>
+    </div>
+    <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
+      <div>
+        <div class="uk-card uk-card-small uk-card-default uk-card-body uk-card-hover">4</div>
+      </div>
+      <div>
+        <div class="uk-card uk-card-small uk-card-default uk-card-body uk-card-hover">5</div>
+      </div>
+      <div>
+        <div class="uk-card uk-card-small uk-card-default uk-card-body uk-card-hover">6</div>
+      </div>
+      <div>
+        <div class="uk-card uk-card-small uk-card-default uk-text-warning uk-card-body uk-card-hover">&times;
+        </div>
+      </div>
+    </div>
+    <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
+      <div>
+        <div class="uk-card uk-card-small uk-card-default uk-card-body uk-card-hover">1</div>
+      </div>
+      <div>
+        <div class="uk-card uk-card-small uk-card-default uk-card-body uk-card-hover">2</div>
+      </div>
+      <div>
+        <div class="uk-card uk-card-small uk-card-default uk-card-body uk-card-hover">3</div>
+      </div>
+      <div>
+        <div class="uk-card uk-card-small uk-card-default uk-text-warning uk-card-body uk-card-hover">&minus;
+        </div>
+      </div>
+    </div>
+    <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
+      <div>
+        <div class="uk-card uk-card-small uk-card-default uk-card-body uk-card-hover">0</div>
+      </div>
+      <div>
+        <div class="uk-card uk-card-small uk-card-default uk-text-warning uk-card-body uk-card-hover">&bullet;</div>
+      </div>
+      <div>
+        <div class="uk-card uk-card-small uk-card-default uk-text-warning uk-card-body uk-card-hover">&equals;</div>
+      </div>
+      <div>
+        <div class="uk-card uk-card-small uk-card-default uk-text-warning uk-card-body uk-card-hover">&plus;
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
