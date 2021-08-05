4fb71ecd05fe16657d094554ae3420e0917898c9
mi-marce7 <michaelmarcelino12@gmail.com>
2021-08-05 18:20:26 -0400

    Implement Mathematical constants. Rearrange Calculator Layout. Clean up HTML

--- frontend/src/components/Calculator.vue
+++ frontend/src/components/Calculator.vue
@@ -1,161 +1,196 @@
 <template>
-<div class="uk-text-right">
-<label class="switch nightModeSwitch">
-  <input class="nightModeSwitch" type="checkbox" unchecked>
-  <span class="slider round"></span>
-</label>
-</div>
-  <div class="uk-container-xsmall uk-align-center">
-    <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
-      <div>
-        <div class="uk-card uk-card-default uk-card-default uk-card-body uk-text-right uk-text-lead">
-          {{ this.equation }}
-        </div>
+   <div class="uk-text-right">
+      <label class="switch nightModeSwitch">
+      <input class="nightModeSwitch" type="checkbox" unchecked>
+      <span class="slider round"></span>
+      </label>
+   </div>
+   <div class="uk-container-xsmall uk-align-center">
+      <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
+         <div>
+            <div class="uk-card uk-card-default uk-card-default uk-card-body uk-text-right uk-text-lead">
+               {{ this.equation }}
+            </div>
+         </div>
       </div>
-    </div>
-    <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
-      <div>
-        <a @click="clear" ref="clear" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-success uk-card-body uk-card-hover">AC</a>
+      <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
+         <div>
+            <a @click="clear" ref="clear" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-success uk-card-body uk-card-hover">AC</a>
+         </div>
+         <div>
+            <a @click="arcos" ref="arcos" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-success uk-card-body uk-card-hover">arccos(x)</a>
+         </div>
+         <div>
+            <a @click="sinh" ref="sinh" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-success uk-card-body uk-card-hover">sinh(x)</a>
+         </div>
+         <div>
+            <a @click="log" ref="log" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-success uk-card-body uk-card-hover">log<sub>b</sub>(x)
+            </a>
+         </div>
       </div>
-      <div>
-        <a @click="arcos" ref="arcos" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-success uk-card-body uk-card-hover">arccos(x)</a>
+      <div class="uk-grid-small uk-child-width-expand@s uk-text-success uk-text-center" uk-grid>
+         <div>
+            <a @click="mad" ref="mad" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-success uk-card-body uk-card-hover">MAD</a>
+         </div>
+         <div>
+            <a @click="standardDeviation" ref="standardDeviation" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-success uk-card-body uk-card-hover">σ(x)</a>
+         </div>
+         <div>
+            <a @click="abPowerX" ref="abPowerX" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-success uk-card-body uk-card-hover">ab<sup>x</sup>
+            </a>
+         </div>
+         <div>
+            <a @click="xPowerY" ref="xPowerY" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-success uk-card-body uk-card-hover">x<sup>y</sup>
+            </a>
+         </div>
       </div>
-      <div>
-        <a @click="sinh" ref="sinh" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-success uk-card-body uk-card-hover">sinh(x)</a>
+      <div class="uk-grid-small uk-child-width-expand@s uk-text-success uk-text-center" uk-grid>
+         <div>
+            <a @click="append('(')" ref="lparen" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-bold uk-text-warning uk-card-body uk-card-hover">&lpar;</a>
+         </div>
+         <div>
+            <a @click="append(')')" ref="rparen" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-bold uk-text-warning uk-card-body uk-card-hover">&rpar;</a>
+         </div>
+         <div>
+            <a @click="append(', ')" ref="comma" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-bold uk-text-warning uk-card-body uk-card-hover">&comma;
+            </a>
+         </div>
+         <div>
+            <a @click="backspace()" ref="backspace" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-success uk-card-body uk-card-hover">DEL
+            </a>
+         </div>
       </div>
-      <div>
-        <a @click="log" ref="log" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-success uk-card-body uk-card-hover">log<sub>b</sub>(x)
-        </a>
+      <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
+         <div>
+            <a @click="pi" ref="clear" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-primary uk-card-body uk-card-hover">&#x03C0;</a>
+         </div>
+         <div>
+            <a @click="e" ref="clear" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-primary uk-card-body uk-card-hover">&#8455;</a>
+         </div>
+         <div>
+            <a @click="g" ref="clear" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-primary uk-card-body uk-card-hover">g</a>
+         </div>
+         <div>
+            <a @click="c" ref="clear" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-primary uk-card-body uk-card-hover">c</a>
+         </div>
       </div>
-    </div>
-    <div class="uk-grid-small uk-child-width-expand@s uk-text-success uk-text-center" uk-grid>
-      <div>
-        <a @click="mad" ref="mad" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-success uk-card-body uk-card-hover">MAD</a>
+      <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
+         <div>
+            <a @click="phi" ref="clear" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-primary uk-card-body uk-card-hover">&#x0278;</a>
+         </div>
+         <div>
+            <a @click="pyth" ref="clear" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-primary uk-card-body uk-card-hover">&#8730;2</a>
+         </div>
+         <div>
+            <a @click="theo" ref="clear" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-primary uk-card-body uk-card-hover">&#8730;3</a>
+         </div>
+         <div>
+            <a @click="ANS" ref="clear" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-warning uk-card-body uk-card-hover">ANS</a>
+         </div>
       </div>
-      <div>
-        <a @click="standardDeviation" ref="standardDeviation" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-success uk-card-body uk-card-hover">σ(x)</a>
+      <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
+         <div>
+            <a @click="append('7')" ref="seven" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-bold uk-card-body uk-card-hover">7</a>
+         </div>
+         <div>
+            <a @click="append('8')" ref="eight" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-bold uk-card-body uk-card-hover">8</a>
+         </div>
+         <div>
+            <a @click="append('9')" ref="nine" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-bold uk-card-body uk-card-hover">9</a>
+         </div>
+         <div>
+            <a @click="append('/')" ref="divide" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-bold uk-text-warning uk-card-body uk-card-hover">
+            &divide;
+            </a>
+         </div>
       </div>
-      <div>
-        <a @click="abPowerX" ref="abPowerX" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-success uk-card-body uk-card-hover">ab<sup>x</sup>
-        </a>
+      <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
+         <div>
+            <a @click="append('4')" ref="four" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-bold uk-card-body uk-card-hover">4</a>
+         </div>
+         <div>
+            <a @click="append('5')" ref="five" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-bold uk-card-body uk-card-hover">5</a>
+         </div>
+         <div>
+            <a @click="append('6')" ref="six" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-bold uk-card-body uk-card-hover">6</a>
+         </div>
+         <div>
+            <a @click="append('*')" ref="multiply" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-warning uk-text-bold uk-card-body uk-card-hover">
+            &times;
+            </a>
+         </div>
       </div>
-      <div>
-        <a @click="xPowerY" ref="xPowerY" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-success uk-card-body uk-card-hover">x<sup>y</sup>
-        </a>
+      <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
+         <div>
+            <a @click="append('1')" ref="one" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-bold uk-card-body uk-card-hover">1</a>
+         </div>
+         <div>
+            <a @click="append('2')" ref="two" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-bold uk-card-body uk-card-hover">2</a>
+         </div>
+         <div>
+            <a @click="append('3')" ref="three" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-bold uk-card-body uk-card-hover">3</a>
+         </div>
+         <div>
+            <a @click="append('-')" ref="minus" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-warning uk-text-bold uk-card-body uk-card-hover">
+            &minus;
+            </a>
+         </div>
       </div>
-    </div>
-    <div class="uk-grid-small uk-child-width-expand@s uk-text-success uk-text-center" uk-grid>
-      <div>
-        <a @click="append('(')" ref="lparen" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-bold uk-text-warning uk-card-body uk-card-hover">&lpar;</a>
+      <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
+         <div>
+            <a @click="append('.')" ref="decimal" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-warning uk-card-body uk-card-hover">&bullet;</a>
+         </div>
+         <div>
+            <a @click="append('0')" ref="zero" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-bold uk-card-body uk-card-hover">0</a>
+         </div>
+         <div>
+            <a @click="evaluate" ref="equals" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-warning uk-text-bold uk-card-body uk-card-hover">
+            &equals;
+            </a>
+         </div>
+         <div>
+            <a @click="append('+')" ref="plus" style="text-decoration: none"
+               class="uk-card uk-card-small uk-card-default uk-text-warning uk-text-bold uk-card-body uk-card-hover">
+            &plus;
+            </a>
+         </div>
       </div>
-      <div>
-        <a @click="append(')')" ref="rparen" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-bold uk-text-warning uk-card-body uk-card-hover">&rpar;</a>
-      </div>
-      <div>
-        <a @click="append(', ')" ref="comma" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-bold uk-text-warning uk-card-body uk-card-hover">&comma;
-        </a>
-      </div>
-      <div>
-        <a @click="backspace()" ref="backspace" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-success uk-card-body uk-card-hover">DEL
-        </a>
-      </div>
-    </div>
-    <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
-      <div>
-        <a @click="append('7')" ref="seven" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-bold uk-card-body uk-card-hover">7</a>
-      </div>
-      <div>
-        <a @click="append('8')" ref="eight" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-bold uk-card-body uk-card-hover">8</a>
-      </div>
-      <div>
-        <a @click="append('9')" ref="nine" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-bold uk-card-body uk-card-hover">9</a>
-      </div>
-      <div>
-        <a @click="append('/')" ref="divide" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-bold uk-text-warning uk-card-body uk-card-hover">
-          &divide;
-        </a>
-      </div>
-    </div>
-    <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
-      <div>
-        <a @click="append('4')" ref="four" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-bold uk-card-body uk-card-hover">4</a>
-      </div>
-      <div>
-        <a @click="append('5')" ref="five" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-bold uk-card-body uk-card-hover">5</a>
-      </div>
-      <div>
-        <a @click="append('6')" ref="six" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-bold uk-card-body uk-card-hover">6</a>
-      </div>
-      <div>
-        <a @click="append('*')" ref="multiply" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-warning uk-text-bold uk-card-body uk-card-hover">
-          &times;
-        </a>
-      </div>
-    </div>
-    <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
-      <div>
-        <a @click="append('1')" ref="one" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-bold uk-card-body uk-card-hover">1</a>
-      </div>
-      <div>
-        <a @click="append('2')" ref="two" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-bold uk-card-body uk-card-hover">2</a>
-      </div>
-      <div>
-        <a @click="append('3')" ref="three" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-bold uk-card-body uk-card-hover">3</a>
-      </div>
-      <div>
-        <a @click="append('-')" ref="minus" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-warning uk-text-bold uk-card-body uk-card-hover">
-          &minus;
-        </a>
-      </div>
-    </div>
-    <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
-      <div>
-        <a @click="append('.')" ref="decimal" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-warning uk-card-body uk-card-hover">&bullet;</a>
-      </div>
-      <div>
-        <a @click="append('0')" ref="zero" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-bold uk-card-body uk-card-hover">0</a>
-      </div>
-      <div>
-        <a @click="evaluate" ref="equals" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-warning uk-text-bold uk-card-body uk-card-hover">
-          &equals;
-        </a>
-      </div>
-      <div>
-        <a @click="append('+')" ref="plus" style="text-decoration: none"
-           class="uk-card uk-card-small uk-card-default uk-text-warning uk-text-bold uk-card-body uk-card-hover">
-          &plus;
-        </a>
-      </div>
-    </div>
-  </div>
-
+   </div>
 </template>
 <script>
 export default {
@@ -197,6 +232,30 @@ export default {
     backspace(){
       this.equation = this.equation.slice(0,-1);
     },
+    e() {
+      this.equation = "2.7182818284"
+    },
+    g() {
+      this.equation = "0.00000000006674"
+    },
+    c() {
+      this.equation = "299792458"
+    },
+     pi() {
+      this.equation = "3.1415926536"
+    },
+     phi() {
+      this.equation = "1.6180339887"
+    },
+     pyth() {
+      this.equation = "1.4142135623"
+    },
+     theo() {
+      this.equation = "1.7320508075"
+    },
+    ANS() {
+      this.equation = this.expression
+    },    
     async evaluate() {
       this.expression = this.equation.replace(/[/]/mg, 'divide')
       let response = await fetch('http://127.0.0.1:8000/' + this.expression + "/");
